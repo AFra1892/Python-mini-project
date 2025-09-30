46f4a23972297bdc53a839ea905383c617e018f8
mashhadi <liverpool1892ynwaaa>
2025-09-30 09:11:43 -0900

    2

--- word-counter.py
+++ word-counter.py
@@ -1,3 +1,10 @@
 #this is the first project of python course
 
-a = 1
+#how to read and print a txt file
+f = open('liverpoolhistory.txt' , 'r')
+content = f.read()
+#print(content)
+
+list_of_words = content.split()
+print(list_of_words)
+print(len(list_of_words))
