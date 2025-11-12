dfb2b2d6fd89427e9d9900708998f8f47c58f6e4
mashhadi <liverpool1892ynwaaa>
2025-11-12 14:02:17 -0900

    oop2

--- oop-practise.py
+++ oop-practise.py
@@ -1,14 +1,22 @@
 class SuperHero:
-    name = ''
-    height = ''
-    city = ''
     
+    sound = 'Poww' #class attribute
+
+    def __init__(self , name , city):
+        self.name = name   # instance attribute
+        self.city = city
+
+    def __str__(self):
+        return f"{self.name} is the hero of {self.city} city"
+
+    #class methods
     def get_name(self):
         print(self.name)
     
 
-first_super_hero = SuperHero()
-first_super_hero.name = 'Batman'
+first_super_hero = SuperHero("batman" , "gotham")
+
 
 first_super_hero.get_name()
-print(first_super_hero.city)
+print(first_super_hero.city) #accessing the class
+print(first_super_hero)
