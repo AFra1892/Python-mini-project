78c8ab57bb969a3e07ad32ff8fe23ed5dbb680bf
mashhadi <liverpool1892ynwaaa>
2025-11-14 10:48:58 -0900

    inheritance

--- oop-practise.py
+++ oop-practise.py
@@ -2,8 +2,9 @@ class SuperHero:
     
     sound = 'Poww' #class attribute
 
-    def __init__(self , name , city):
+    def __init__(self , name , city , power):
         self.name = name   # instance attribute
+        self.power = power
         self.city = city
 
     def __str__(self):
@@ -13,10 +14,14 @@ class SuperHero:
     def get_name(self):
         print(self.name)
     
+class SideKick(SuperHero):
+    def get_power(self):
+        return self.power
 
-first_super_hero = SuperHero("batman" , "gotham")
 
-
+first_super_hero = SuperHero("batman" , "gotham" , 'being-rich')
+first_sideKick = SideKick('robin' , 'gotham' , 'no-power')
 first_super_hero.get_name()
 print(first_super_hero.city) #accessing the class
 print(first_super_hero)
+print(first_sideKick.get_power())
