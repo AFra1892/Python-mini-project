53dcfc095f7b4b3ab9964e0b8e1604301aad5fb2
mashhadi <liverpool1892ynwaaa>
2025-11-21 20:50:12 -0900

    numpy methods

---
+++ numpy-practise.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([1,2,3])
+
+b = np.array([[1,2,3],[4,5,6]])
+print(b)
+# number of dimention
+print(a.ndim)
+print(b.shape)
+print(a.dtype)
+
+#accessing and changing elements
