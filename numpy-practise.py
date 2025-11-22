8119b938bb59a69f4603de10cd07a74ff33f48d9
mashhadi <liverpool1892ynwaaa>
2025-11-21 21:00:07 -0900

    accessing

--- numpy-practise.py
+++ numpy-practise.py
@@ -3,10 +3,18 @@ import numpy as np
 a = np.array([1,2,3])
 
 b = np.array([[1,2,3],[4,5,6]])
-print(b)
+#print(b)
 # number of dimention
-print(a.ndim)
-print(b.shape)
-print(a.dtype)
+#print(a.ndim)
+#print(b.shape)
+#print(a.dtype)
 
 #accessing and changing elements
+c = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#print(c)
+#print(c.shape)
+
+#print(c[:,1:2])
+
+c[1,2] = 99
+print(c)
