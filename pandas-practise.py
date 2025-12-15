d71b0efc5db3c365e61675f74cb66f1aeef40446
mashhadi <liverpool1892ynwaaa>
2025-12-15 14:42:50 -0900

    mathplt

--- pandas-practise.py
+++ pandas-practise.py
@@ -3,5 +3,12 @@ import pandas as pd
 df = pd.read_csv('orders.csv')
 #print(df)
 
-print(df.columns)
-print(df.iloc[10]['Shipped'])
+#print(df.columns)
+#print(df.iloc[10]['Shipped'])
+
+#print(df.tail())
+
+#print(df[df['Country'] == "USA" & (df['OrderDate'] == '2024-06-01')] )
+
+filt = (df['Price'] > 100) | (df['Country'] == 'UAE') 
+print(df[filt])
