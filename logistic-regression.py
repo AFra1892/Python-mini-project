a9a21a2b691afa77db286436e34df84c11dfc057
mashhadi <liverpool1892ynwaaa>
2026-02-25 15:27:13 -0900

    multinomial Logistic regression

--- logistic-regression.py
+++ logistic-regression.py
@@ -11,4 +11,18 @@ clf = LogisticRegression(max_iter=10000 , random_state=0)
 clf.fit(X_train,y_train)
 
 acc = accuracy_score(y_test , clf.predict(X_test)) * 100
-print(f"Logistic Regression model accuracy: {acc:.2f}%")
+#print(f"Logistic Regression model accuracy: {acc:.2f}%")
+
+
+from sklearn import datasets,linear_model,metrics
+
+digits = datasets.load_digits()
+X = digits.data
+y = digits.target
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.4 , random_state=1)
+
+reg =  linear_model.LogisticRegression(max_iter=10000 , random_state=0)
+reg.fit(X_train , y_train)
+y_pred = reg.predict(X_test)
+
+print(f"Logistic Regression model accuracy: {metrics.accuracy_score(y_test, y_pred) * 100:.2f}%")
